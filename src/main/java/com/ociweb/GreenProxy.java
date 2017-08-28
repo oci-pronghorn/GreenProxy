472f2be756380e26da432c973ff1a71ecf5cca06
David Giovannini <dave@David.local>
2017-08-28 12:20:08 -0500

    initial complaint

---
+++ src/main/java/com/ociweb/GreenProxy.java
@@ -0,0 +1,23 @@
+package com.ociweb;
+
+import com.ociweb.gl.api.Builder;
+import com.ociweb.gl.api.GreenApp;
+import com.ociweb.gl.api.GreenRuntime;
+
+public class GreenProxy implements GreenApp
+{
+    @Override
+    public void declareConfiguration(Builder builder) {
+
+
+        
+    }
+
+
+    @Override
+    public void declareBehavior(GreenRuntime runtime) {
+       
+    	
+
+    }
+}
